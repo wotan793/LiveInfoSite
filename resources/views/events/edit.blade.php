60ad71caeebe9c178ed9f348e85cf785ecef1e39
K.N <es9431ms@gmail.com>
2018-09-16 19:28:12 +0000

    更新ボタンのレイアウト変更

--- resources/views/events/edit.blade.php
+++ resources/views/events/edit.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div id="event">
     <h1>イベント編集ページ</h1>
-    
     <div class="row">
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
@@ -49,9 +48,11 @@
                 </div>
 
 
-            {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+            {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 
             {!! Form::close() !!}
         </div>
     </div>
+</div>
+
 @endsection
