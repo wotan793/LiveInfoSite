281e0d21d66a43aae67c69a6f3b15b4142903d56
K.N <es9431ms@gmail.com>
2018-09-16 19:39:04 +0000

    興味あり、参加ボタンを押したイベントを表示するためのview

---
+++ resources/views/events/myevents.blade.php
@@ -0,0 +1,35 @@
+@if ($events)
+    <div class="row">
+        @foreach ($events as $key => $event)
+            <div class="myevent">
+                <div class="col-md-3 col-sm-4 col-xs-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center">
+                            <img src="{{ $event->event_imageUrl }}" width="230" height="300">
+                        </div>
+                        <div class="panel-body">
+                            @if ($event->id)
+                            <p>
+                                <span class="glyphicon glyphicon-time"></span>
+                                {{ $event->event_date}}
+                            </p>
+                            <p>
+                                <span class="glyphicon glyphicon-map-marker"></span>
+                                {{ $event->event_venue}}&nbsp;{{ $event->event_prefecture}}
+                            </p>
+                                <p class="event-title"><a href="{{ route('events.show', $event->id) }}">{{ $event->event_name }}</a></p>
+                            @else
+                                <p class="event-title">{{ $event->event_name }}</p>
+                            @endif
+                            <div class="buttons text-center">
+                                @if (Auth::check())
+                                    @include('event_user.event_interested_button', ['event' => $event])
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endif
