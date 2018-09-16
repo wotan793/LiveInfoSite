ec817d0b4b99e5eb9eaa36d7618dafbec1eec851
K.N <es9431ms@gmail.com>
2018-09-16 19:37:34 +0000

    validationの日本語表記化

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,18 @@
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'メールアドレスまたはパスワードが誤っているため、ログインできません。',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
